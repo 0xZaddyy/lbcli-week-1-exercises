ca02da8db760aa078b98e6281c6f1dc17445341c
Oladapo Oyindamola <111582215+0xZaddyy@users.noreply.github.com>
2026-03-13 10:01:39 +0100

    add network flag to scripts

--- submission/04.sh
+++ submission/04.sh
@@ -1,2 +1,6 @@
 # Create a wallet with the name "builderswallet".
-bitcoin-cli createwallet "builderswallet"
+bitcoin-cli --regtest createwallet "builderswallet"
+
+# Generate an address and mine 101 blocks to it to get initial balance
+ADDRESS=$(bitcoin-cli --regtest -rpcwallet=builderswallet getnewaddress)
+bitcoin-cli --regtest generatetoaddress 101 $ADDRESS > /dev/null
